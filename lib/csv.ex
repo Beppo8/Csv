72f5742b0f198d022786934f28220007866d37a4
beppo <beppo@localhost.localdomain>
2020-12-24 11:29:39 -0600

    Parcing CSV

--- lib/csv.ex
+++ lib/csv.ex
@@ -1,20 +1,27 @@
 defmodule Csv do
-  @moduledoc """
-  Documentation for `Csv`.
-  """
 
-  @doc """
-  Hello world.
+  @clearbit_api_key "sk_1234_abcd"
 
-  ## Examples
+  def email_status_from_csv!(filename \\ "email_data.csv") do
+    filename
+    |> File.stream!()
+    |> CSV.decode!(headers: true)
+    |> Enum.each(&get_status!/1)
+  end
 
-      iex> Csv.hello()
-      :world
+  defp get_status(%{"Email Address" => email, "CONFIRM_IP" => ip}) do
+    high_risk = "https://risk.clearbit.com/v1/calculate?email=#{email}&ip=#{ip}"
+      |> HTTPoison.get([headers: [Authorization: "Bearer #{@clearbit_api_key}"]])
+      |> parse_response!()
+      |> high_risk?()
 
-  """
+  end
 
-  filename |> File.stream!() |> CSV.decode() |> Enum.to_list()
-  def hello do
-    :world
+  defp high_risk?(response) do
+    get_in(response, ["risk", "level"]) == "high"
+  end
+
+  defp parse_response!(%HTTPoison.Response{body: body}) do
+    Jason.decode!(body)
   end
 end
